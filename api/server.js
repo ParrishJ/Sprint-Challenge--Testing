19793df9b9f6aa2bfc7d62f17759e0d92171c633
ParrishJ <jarparrish@gmail.com>
2019-06-21 09:47:11 -0700

    all tests besides 422 error check passing

--- api/server.js
+++ api/server.js
@@ -8,7 +8,28 @@ server.use(express.json());
 
 
 
-//endpoints here;
+server.get('/games', (req, res) => {
+    Games.getGames()
+        .then(game => {
+            res.status(200).json(game)
+        })
+        .catch(error => {
+            res.status(500).json(error)
+        });
+});
+
+server.post('/games', (req, res) => {
+    Games.addGame(req.body)
+        .then(ids => {
+            if (req.body.title && req.body.genre) {
+                res.status(200).json(ids)
+            } else
+                res.status(422).json({ error: "Required data missing from post" })
+        })
+        .catch(error => {
+            res.status(500).json(error)
+        })
+})
 
 
 module.exports = server;
--- games/gamesModel.spec.js
+++ games/gamesModel.spec.js
@@ -60,3 +60,4 @@ describe('game model', () => {
         })
     })
 })
+
