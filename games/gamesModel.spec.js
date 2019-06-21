7172330f2ac9e91c3225528517b8b28472ccfd51
ParrishJ <jarparrish@gmail.com>
2019-06-21 09:10:45 -0700

    first test checking for .env implemented and passed

--- games/gamesModel.spec.js
+++ games/gamesModel.spec.js
@@ -1,7 +1,15 @@
 const db = require('../data/dbConfig.js');
 const supertest = require('supertest');
-const server = require('..api/server')
+const server = require('../api/server')
 
 const { addGame, getGames } = require('./gamesModel')
 
-//tests go here
+describe('game model', () => {
+    beforeEach(async () => {
+        await db('gameTable').truncate();
+    });
+
+    it('should set testing env variable', () => {
+        expect(process.env.DB_ENV).toBe('testing');
+    });
+})
